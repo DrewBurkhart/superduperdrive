cf912714a5fe29cc7540f565507660faf6f973c5
Andrew Burkhart <andrew@edgeperformance.io>
2021-07-24 12:29:18 -0700

    refactor: Remove unused import

--- src/test/java/com/udacity/jwdnd/course1/cloudstorage/NoteTests.java
+++ src/test/java/com/udacity/jwdnd/course1/cloudstorage/NoteTests.java
@@ -9,7 +9,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class NoteTests {
