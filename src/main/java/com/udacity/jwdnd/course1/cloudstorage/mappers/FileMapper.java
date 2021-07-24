f9a62d8066b05f10ac71bd1d15c627e9592bbbcb
Andrew Burkhart <andrew@edgeperformance.io>
2021-07-23 21:29:52 -0700

    feat: Add file models and mapper

--- src/main/java/com/udacity/jwdnd/course1/cloudstorage/mappers/FileMapper.java
+++ src/main/java/com/udacity/jwdnd/course1/cloudstorage/mappers/FileMapper.java
@@ -1,2 +1,28 @@
-package com.udacity.jwdnd.course1.cloudstorage.mappers;public class FileMapper {
+package com.udacity.jwdnd.course1.cloudstorage.mappers;
+
+import com.udacity.jwdnd.course1.cloudstorage.model.File;
+import org.apache.ibatis.annotations.*;
+
+import java.util.List;
+
+@Mapper
+public interface FileMapper {
+    // CREATE
+    @Insert("INSERT INTO FILES (filename, contenttype, filesize, userid, filedata) " +
+            "VALUES(#{fileName}, #{contentType}, #{fileSize}, #{userId}, #{fileData})")
+    @Options(useGeneratedKeys = true, keyProperty = "fileId")
+    void addFile(File file);
+
+    // READ
+    @Select("SELECT * FROM FILES WHERE userid = #{userId}")
+    List<String> getFilesByUser(Integer userId);
+
+    @Select("SELECT * FROM FILES WHERE fileid = #{fileId}")
+    File getFileById(Integer fileId);
+
+    // UPDATE
+
+    // DELETE
+    @Delete("DELETE FROM FILES WHERE fileid = #{fileId}")
+    void deleteFile(Integer fileId);
 }
--- src/main/java/com/udacity/jwdnd/course1/cloudstorage/model/FileForm.java
+++ src/main/java/com/udacity/jwdnd/course1/cloudstorage/model/FileForm.java
@@ -1,2 +1,15 @@
-package com.udacity.jwdnd.course1.cloudstorage.model;public class FileForm {
+package com.udacity.jwdnd.course1.cloudstorage.model;
+
+import org.springframework.web.multipart.MultipartFile;
+
+public class FileForm {
+    private MultipartFile file;
+
+    public MultipartFile getFile() {
+        return file;
+    }
+
+    public void setFile(MultipartFile file) {
+        this.file = file;
+    }
 }
