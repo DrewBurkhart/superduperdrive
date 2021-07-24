7cc50cd3aa8bde4f144c9fbfffb56a094b756731
Andrew Burkhart <andrew@edgeperformance.io>
2021-07-24 09:07:07 -0700

    feat: Add redirect and error messaging
    When a user signs up successfully, redirect
    them to the login page with a success
    message. When a user attempts to sign up with
    a username that is already taken, show them
    a message on the signup page.
    
    Resolves #8

--- src/main/java/com/udacity/jwdnd/course1/cloudstorage/controllers/SignupController.java
+++ src/main/java/com/udacity/jwdnd/course1/cloudstorage/controllers/SignupController.java
@@ -41,7 +41,7 @@ public class SignupController {
         }
 
         if (signupError == null) {
-            model.addAttribute("signupSuccess", true);
+            return "redirect:/login?success";
         } else {
             model.addAttribute("signupError", signupError);
         }
